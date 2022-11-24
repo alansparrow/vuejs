b9224870bd8f6217c520741464be124d3b35a80f
Trung Tran <trannguyenbaotrung@gmail.com>
2022-11-24 14:07:08 +0700

    Clean Section 1

---
+++ alias.ps1
@@ -0,0 +1,8 @@
+function Git-Add {git add .}
+Set-Alias -Name gadd -Value Git-Add
+
+function Git-Commit {git commit}
+Set-Alias -Name gcom -Value Git-Commit
+
+function Git-Push {git push}
+Set-Alias -Name pp -Value Git-Push
