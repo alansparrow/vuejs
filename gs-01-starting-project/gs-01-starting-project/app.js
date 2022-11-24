7269588f15909f5d43553f47d594f6d5516d57ad
Trung Tran <trannguyenbaotrung@gmail.com>
2022-11-24 13:31:28 +0700

    Add Vue

--- gs-01-starting-project/gs-01-starting-project/app.js
+++ gs-01-starting-project/gs-01-starting-project/app.js
@@ -1,13 +1,29 @@
-const buttonElem = document.querySelector('button');
-const inputElem = document.querySelector('input');
-const listElem = document.querySelector('ul');
+Vue.createApp({
+    data() {
+        return {
+            goals: [],
+            enteredValue: ''
+        };
+    },
+    methods: {
+        addGoal() {
+            this.goals.push(this.enteredValue);
+            this.enteredValue = '';
+        }
+    }
+}).mount('#app');
 
-function addGoal() {
-    const enteredValue = inputElem.value;
-    const listItemElem = document.createElement('li');
-    listItemElem.textContent = enteredValue;
-    listElem.appendChild(listItemElem);
-    inputElem.value = '';
-}
+// const buttonElem = document.querySelector('button');
+// const inputElem = document.querySelector('input');
+// const listElem = document.querySelector('ul');
 
-buttonElem.addEventListener('click', addGoal)
+// function addGoal() {
+//     const enteredValue = inputElem.value;
+//     const listItemElem = document.createElement('li');
+//     listItemElem.textContent = enteredValue;
+//     listElem.appendChild(listItemElem);
+//     inputElem.value = '';
+// }
+
+// buttonElem.addEventListener('click', addGoal)
+
