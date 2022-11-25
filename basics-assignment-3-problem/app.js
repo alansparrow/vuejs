131c535aec774d11e0c293485469fc68a4221ebf
Trung Tran <trannguyenbaotrung@gmail.com>
2022-11-25 15:18:29 +0700

    Do it diff way!

--- basics-assignment-3-problem/app.js
+++ basics-assignment-3-problem/app.js
@@ -1,19 +1,12 @@
 const app = Vue.createApp({
     data() {
         return {
-            result: '',
             val: 0
         }
     },
     watch: {
         val(value) {
-            console.log(value);
-            if (value < 37) {
-                this.result = 'Not there yet!';
-            } else if (value > 37) {
-                this.result = 'Too much!';
-            } else {
-                this.result = 37;
+            if (value == 37) {
                 setTimeout(() => {
                     this.val = 0;
                 }, 5000);
@@ -21,7 +14,16 @@ const app = Vue.createApp({
         }
     },
     computed: {
-
+        result() {
+            console.log(this.val);
+            if (this.val < 37) {
+                return 'Not there yet!';
+            } else if (this.val > 37) {
+                return 'Too much!';
+            } else {
+                return this.val;
+            }
+        }
     },
     methods: {
         add1() {
