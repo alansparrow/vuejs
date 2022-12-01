474ee01c5ad870ce67d636296d9279a94da5cf1e
Trung Tran <trannguyenbaotrung@gmail.com>
2022-12-01 12:42:05 +0700

    Use ref

--- behind-scenes-01-starting-setup/app.js
+++ behind-scenes-01-starting-setup/app.js
@@ -12,6 +12,7 @@ const app = Vue.createApp({
     setText() {
       // this.message = this.currentUserInput;
       console.dir(this.$refs.userText);
+      this.message = this.$refs.userText.value;
     },
   },
 });
