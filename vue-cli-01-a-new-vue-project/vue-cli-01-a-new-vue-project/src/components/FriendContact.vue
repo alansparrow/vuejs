6ac3434670646c1190902353f0c14a68c6eb02ca
Trung Tran <trannguyenbaotrung@gmail.com>
2022-12-05 13:42:52 +0700

    Added a component

---
+++ vue-cli-01-a-new-vue-project/vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
@@ -0,0 +1,32 @@
+<template>
+  <li>
+    <h2>{{ friend.name }}</h2>
+    <button @click="toggleDetails">{{ detailsAreVisible ? 'Hide' : 'Show' }}</button>
+    <ul v-if="detailsAreVisible">
+      <li><strong>Phone: </strong> {{ friend.phone }}</li>
+      <li><strong>Email: </strong> {{ friend.email }}</li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend: {
+        id: "julie",
+        name: "Julie Jones",
+        phone: "09876 543 221",
+        email: "julie@localhost.com",
+      },
+    };
+  },
+  methods: {
+    toggleDetails() {
+        this.detailsAreVisible = !this.detailsAreVisible;
+    }
+  }
+};
+</script>
+ 
