b3c0675a25c3df55b0b3e142dcf5382ba28e8fb5
Trung Tran <trannguyenbaotrung@gmail.com>
2022-12-28 05:09:54 +0700

    Scoped slot

--- cmp-adv-01-starting-setup/src/App.vue
+++ cmp-adv-01-starting-setup/src/App.vue
@@ -7,6 +7,13 @@
       :info-text="activeUser.description"
       :role="activeUser.role"
     ></user-info>
+    <course-goals>
+      <template #default="slotProps">
+        <h2>{{ slotProps.item }}</h2>
+        <p>{{ slotProps.otherProp }}</p>
+        <p>{{ slotProps }}</p>
+      </template>
+    </course-goals>
   </div>
 </template>
 
@@ -14,12 +21,14 @@
 import UserInfo from './components/UserInfo.vue';
 import TheHeader from './components/TheHeader.vue';
 import BadgeList from './components/BadgeList.vue';
+import CourseGoals from './components/CourseGoals.vue';
 
 export default {
   components: {
     UserInfo,
     TheHeader,
-    BadgeList
+    BadgeList,
+    CourseGoals
   },  
   data() {
     return {
