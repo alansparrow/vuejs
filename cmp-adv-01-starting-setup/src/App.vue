ce410e0c0ef545357f04a3efd795a434276c7484
Trung Tran <trannguyenbaotrung@gmail.com>
2023-01-02 13:35:24 +0700

    Keep Dynamic Components Alive

--- cmp-adv-01-starting-setup/src/App.vue
+++ cmp-adv-01-starting-setup/src/App.vue
@@ -3,36 +3,38 @@
     <the-header></the-header>
     <button @click="setSelectedComponent('active-goals')">Active Goals</button>
     <button @click="setSelectedComponent('manage-goals')">Manage Goals</button>
-    <component :is="selectedComponent"></component>
+    <keep-alive>
+      <component :is="selectedComponent"></component>
+    </keep-alive>
   </div>
 </template>
 
 <script>
-import TheHeader from './components/TheHeader.vue';
-import ActiveGoals from './components/ActiveGoals.vue';
-import ManageGoals from './components/ManageGoals.vue';
+import TheHeader from "./components/TheHeader.vue";
+import ActiveGoals from "./components/ActiveGoals.vue";
+import ManageGoals from "./components/ManageGoals.vue";
 
 export default {
   components: {
     TheHeader,
     ActiveGoals,
-    ManageGoals
-  },  
+    ManageGoals,
+  },
   data() {
     return {
-      selectedComponent: 'active-goals',
+      selectedComponent: "active-goals",
       activeUser: {
-        name: 'Maximilian Schwarzmüller',
-        description: 'Site owner and admin',
-        role: 'admin',
+        name: "Maximilian Schwarzmüller",
+        description: "Site owner and admin",
+        role: "admin",
       },
     };
   },
   methods: {
     setSelectedComponent(cpn) {
       this.selectedComponent = cpn;
-    }
-  }
+    },
+  },
 };
 </script>
 
